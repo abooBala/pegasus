5c999497eb701322f8198fbbd4165d3cb3a88a7e
abooBala <aboobala64@gmail.com>
2024-05-05 08:58:10 +0100

    Added default header and footer tags

---
+++ header.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Header template
+ * 
+ * @package pegasus
+ * 
+ */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes() ?>">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ) ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header>Header</header>
