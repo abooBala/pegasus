4b90769609168789904c8e9517b57311282e3560
abooBala <aboobala64@gmail.com>
2024-05-05 12:05:50 +0100

    Added sytlesheet and autoreload

--- functions.php
+++ functions.php
@@ -6,4 +6,13 @@
  * @package Pegasus
  * 
  */
+
+ function pegasus_enqueue_scripts() {
+
+    wp_enqueue_style("stylesheet", get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css') );
+
+ }
+
+ add_action( 'wp_enqueue_scripts','pegasus_enqueue_scripts' );
+
 ?>
